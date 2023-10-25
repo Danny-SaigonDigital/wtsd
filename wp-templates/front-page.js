c9ac957d68bb0aa16d2bf216b34dc59b69e7552e
danny <tnd.290800@gmail.com>
2023-10-25 11:54:08 +0700

    change env

--- wp-templates/front-page.js
+++ wp-templates/front-page.js
@@ -34,6 +34,7 @@ export default function Component() {
           <Hero title={'Front Page'} />
           <div className="text-center">
             <p>This page is utilizing the "front-page" WordPress template.</p>
+            <p>alo alo</p>
             <code>wp-templates/front-page.js</code>
           </div>
         </Container>
